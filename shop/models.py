a0747fd24c0f86361d05c0bb1e384b0564ce3faf
soffia1407 <sff140705@gmail.com>
2024-12-25 03:28:15 +0700

    для главной страницы инфа

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Название товара")
+    image = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name="Изображение товара")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание товара")
+
+
+    def __str__(self):
+        return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def home(request):
+    # Получаем последние 5 добавленных товаров
+    latest_products = Product.objects.order_by('-id')[:5]  #  '-id' для сортировки от новых к старым
+
+    context = {
+        'latest_products': latest_products,
+    }
+
+    return render(request, 'shop/home.html', context)
+
+
+def register(request): # добавили функцию
+    return render(request, 'shop/register.html')
+
+def login_view(request):
+    return render(request, 'shop/login.html')
+
+def service(request):
+    return render(request, 'shop/service.html')
